4a2f3d23acb9cdb73a5a3348fe76c3309b6a82d6
LIGHTscrn <pranavprasad.r500@gmail.com>
2026-01-16 19:37:09 +0530

    Updated

--- lua/pranav/lazyconfig.lua
+++ lua/pranav/lazyconfig.lua
@@ -1,9 +1,12 @@
 return {
     -- Telescope
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim', tag = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
     },
 
     -- Rose-pine theme
